a8b2da557ef3135bd48dee2a56e79ab0d9b42109
patrickwwewe <patrick.mobil@gmx.at>
2025-11-28 17:54:06 +0100

    Add GUI version of Pascal project
    Introduces a new GUI version of the Pascal project, including source files, form definitions, project configuration, and compiled binaries. Also updates project session file to reflect new units and editor state.

---
+++ pascal lernen/GUI VERSION/unit1.pas
@@ -0,0 +1,61 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Memo1: TMemo;
+    procedure Button1Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+  hoehe, breite : Integer;
+  sHoehe, sBreite : String;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  bhoehe, bbreite : Integer;
+begin
+  Memo1.Clear;
+   // Texte aus den Edit-Feldern holen
+  sHoehe  := Edit1.Text;
+  sBreite := Edit2.Text;
+  // Versuch: Text aus den Edits in Zahlen umwandeln
+  if TryStrToInt(Edit1.Text, bHoehe) and TryStrToInt(Edit2.Text, bBreite) then
+  begin
+    Memo1.Lines.Add('Eingegebene Werte:');
+    Memo1.Lines.Add('Höhe  : ' + (shoehe) + ' mm');
+    Memo1.Lines.Add('Breite: ' + (sbreite) + ' mm');
+  end
+  else
+  begin
+    Memo1.Lines.Add('Fehler: Bitte gültige ganze Zahlen eingeben!');
+  end;
+end;
+
+end.
+
